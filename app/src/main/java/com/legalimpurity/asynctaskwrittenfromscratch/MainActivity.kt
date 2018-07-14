dd965d2a7268bcadd4d392fe61c57397d5bd639d
rajatkhanna <legalimpurity@gmail.com>
2018-07-14 21:05:26 +0530

    Step 11, modify MyAsyncTaskImpl class to run the actual task.

--- app/src/main/java/com/legalimpurity/asynctaskwrittenfromscratch/MainActivity.kt
+++ app/src/main/java/com/legalimpurity/asynctaskwrittenfromscratch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.legalimpurity.asynctaskwrittenfromscratch
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
             AndroidAsyncTaskImpl(this).execute("http://www.mocky.io/v2/5b49ac2b3100005a008bc03b")
         }
         myAsyncTaskButton.setOnClickListener {
-            MyAsyncTaskImpl().execute()
+            MyAsyncTaskImpl(WeakReference(this),"http://www.mocky.io/v2/5b49ac2b3100005a008bc03b").execute()
         }
     }
 }
--- app/src/main/java/com/legalimpurity/asynctaskwrittenfromscratch/MyAsyncTaskImpl.kt
+++ app/src/main/java/com/legalimpurity/asynctaskwrittenfromscratch/MyAsyncTaskImpl.kt
@@ -2,15 +2,34 @@ package com.legalimpurity.asynctaskwrittenfromscratch
 
 import android.util.Log
 import com.legalimpurity.asynctaskwrittenfromscratch.MyAsyncTaskCode.MyAsyncTask
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.Reader
+import java.lang.ref.WeakReference
+import java.net.HttpURLConnection
+import java.net.URL
 
-class MyAsyncTaskImpl : MyAsyncTask<String>()
+class MyAsyncTaskImpl(private val weakReferenceMainActivity : WeakReference<MainActivity>, private val urlString:String) : MyAsyncTask<String>()
 {
     val LOG_TAG = "MyAsyncTask_Logs"
 
     override fun doInBackground(): String {
         Log.d(LOG_TAG,"doInBackgroundCalledOn: "+Thread.currentThread().name)
-        return ""
-    }
+        var returnString = "ERRORSTRING"
+        val url = URL(urlString)
+        val urlConnection = url.openConnection() as HttpURLConnection
+        try {
+            val inputStream = BufferedInputStream(urlConnection.inputStream)
+
+            val bufferedReader  = BufferedReader(InputStreamReader(inputStream) as Reader?)
+            returnString = bufferedReader.readLine()
+
+        }finally {
+            urlConnection.disconnect()
+        }
+        return returnString    }
 
     override fun onPreExecute() {
         Log.d(LOG_TAG,"onPreExecuteCalledOn: "+Thread.currentThread().name)
@@ -18,6 +37,8 @@ class MyAsyncTaskImpl : MyAsyncTask<String>()
 
     override fun onPostExecute(valueFromDoInBackground: String) {
         Log.d(LOG_TAG,"onPostExecuteCalledOn: "+Thread.currentThread().name)
+        Log.d(LOG_TAG, "taskOutput: $valueFromDoInBackground")
+        weakReferenceMainActivity.get()?.asyncTaskOutputTextView?.text = valueFromDoInBackground
     }
 
 }
